8cf5097cf624961f11f3021c1787e48714999fe7
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-03 18:41:15 +0000

    Revert "day 2 part 1 done, part 2 wip"
    This reverts commit 3f32e28f98163be50c2f22632940a06cfcc4826d.

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -27,7 +27,11 @@ func getReports(lines []string) [][]int {
 func isReportSafe(report []int) bool {
 	op := isIncreasingOrDecreasing(report, 0, 1)
 
-	for i := 0; i < len(report)-1; i++ {
+	for i := 0; i < len(report); i++ {
+		if i+1 == len(report) {
+			break
+		}
+
 		diff := math.Abs(float64(report[i] - report[i+1]))
 		if diff > 3 || diff == 0 {
 			return false
@@ -42,11 +46,10 @@ func isReportSafe(report []int) bool {
 			if report[i] < report[i+1] {
 				return false
 			}
-		default:
-			return false
 		}
 
 	}
+	fmt.Println("Safe")
 	return true
 }
 
@@ -75,48 +78,17 @@ func partOne(lines []string) int {
 	return count
 }
 
-func partTwo(lines []string) int {
-	reports := getReports(lines)
-
-	count := 0
-	for _, report := range reports {
-		if isReportSafeV2(report) {
-			count++
-		}
-		fmt.Println()
-	}
-
-	return count
-}
-
-func isReportSafeV2(report []int) bool {
-	// check if the report is already safe
-	if isReportSafe(report) {
-		fmt.Printf("%v is safe\n", report)
-		return true
-	}
-
-	fmt.Println(report)
-	for i := 0; i < len(report); i++ {
-		// delete the combination position to remove
-		newReport := u.DeleteAtIndex(report, i)
-
-		fmt.Printf("%v <-- Variation: %v\n", report[i], newReport)
-		if isReportSafe(newReport) {
-			return true
-		}
-	}
-
-	return false
-}
-
 func main() {
 	u.RenderDayHeader(2)
-	lines, err := u.ReadLines("cmd/day2/example.txt")
+	lines, err := u.ReadLines("cmd/day2/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// fmt.Printf("Part 1: %v\n", partOne(lines))
-	fmt.Printf("Part 2: %v\n", partTwo(lines))
+	fmt.Printf("Part 1: %v\n", partOne(lines))
+	// fmt.Printf("Part 2: %v\n", partTwo(lines))
+}
+
+func partTwo(lines []string) int {
+	panic("unimplemented")
 }
