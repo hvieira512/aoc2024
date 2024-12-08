1dd3b868035bdd96b37da6ad326c7a0314702a4c
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-08 16:56:05 +0000

    day 5 part 1 restart

--- cmd/day5/main.go
+++ cmd/day5/main.go
@@ -8,53 +8,46 @@ import (
 	"github.com/hvieira512/aoc2024/cmd/utils"
 )
 
-func getContents(lines []string) ([][]int, [][]int) {
-	getIdx := func() int {
-		idx := -1
-		for i := range lines {
-			if lines[i] == "" {
-				idx = i
-			}
+func parseInput(lines []string) ([][2]int, [][]int) {
+	rules := [][2]int{}
+	updates := [][]int{}
+
+	idx := -1
+	for i := range lines {
+		if len(lines[i]) == 0 {
+			idx = i
 		}
-		return idx
 	}
-
-	getValuesSep := func(a, b int, sep string) [][]int {
-		values := [][]int{}
-		for i := a; i < b; i++ {
-			strAux := strings.Split(lines[i], sep)
-
-			lineValue := []int{}
-			for j := range strAux {
-				nvalue, _ := strconv.Atoi(strAux[j])
-				lineValue = append(lineValue, nvalue)
-			}
-			values = append(values, lineValue)
+	for i := 0; i < idx; i++ {
+		pagesRuleStr := strings.Split(lines[i], "|")
+		fPageRule, _ := strconv.Atoi(pagesRuleStr[0])
+		lPageRule, _ := strconv.Atoi(pagesRuleStr[1])
+		rules = append(rules, [2]int{fPageRule, lPageRule})
+	}
+	for i := idx + 1; i < len(lines); i++ {
+		update := []int{}
+		pagesUpdateStr := strings.Split(lines[i], ",")
+		for _, pageUpdateStr := range pagesUpdateStr {
+			page, _ := strconv.Atoi(pageUpdateStr)
+			update = append(update, page)
 		}
-		return values
+		updates = append(updates, update)
 	}
-
-	idx := getIdx()
-	rules := getValuesSep(0, idx, "|")
-	updates := getValuesSep(idx, len(lines), ",")
 
 	return rules, updates
 }
 
-func partOne(rules [][]int, updates [][]int) int {
-	result := 0
+func partOne(lines []string) int {
+	total := 0
 
-	fmt.Println(rules)
-	for _, update := range updates {
-		fmt.Println(update)
-	}
+	rules, updates := parseInput(lines)
 
-	return result
+	return total
 }
 
 func main() {
+	utils.RenderDayHeader(5)
 	lines, _ := utils.ReadLines("cmd/day5/example.txt")
-	rules, updates := getContents(lines)
 
-	fmt.Printf("Part 1: %v\n", partOne(rules, updates))
+	fmt.Printf("Part 1: %v\n", partOne(lines))
 }
