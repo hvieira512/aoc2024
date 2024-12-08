af6668afb9e589e5031da2a29c96b8fc3269fd47
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-08 00:05:16 +0000

    day 4 part 2 wip

--- cmd/day4/main.go
+++ cmd/day4/main.go
@@ -6,36 +6,74 @@ import (
 	u "github.com/hvieira512/aoc2024/cmd/utils"
 )
 
-var directions = [][2]int{
-	{-1, 0}, {1, 0},
-	{0, -1}, {0, 1},
-	{-1, -1}, {1, 1},
-	{-1, 1}, {1, -1},
+func checkDirection(x, y, dx, dy int, word string, grid [][]rune) bool {
+	rows := len(grid)
+	cols := len(grid[0])
+	wordRunes := []rune(word)
+
+	for i := 0; i < len(word); i++ {
+		nx, ny := x+dx*i, y+dy*i
+		outOfBounds := nx < 0 || ny < 0 || nx >= rows || ny >= cols
+
+		if outOfBounds || grid[nx][ny] != wordRunes[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func partOne(grid [][]rune, word string) int {
 	rows := len(grid)
 	cols := len(grid[0])
-	wordRunes := []rune(word)
 	count := 0
-
-	checkDirection := func(x, y, dx, dy int) bool {
-		for i := 0; i < len(word); i++ {
-			nx, ny := x+dx*i, y+dy*i
-			outOfBounds := nx < 0 || ny < 0 || nx >= rows || ny >= cols
-
-			if outOfBounds || grid[nx][ny] != wordRunes[i] {
-				return false
-			}
-		}
-		return true
+	directions := [][2]int{
+		{-1, 0}, {1, 0},
+		{0, -1}, {0, 1},
+		{-1, -1}, {1, 1},
+		{-1, 1}, {1, -1},
 	}
 
 	for i := range rows {
 		for j := range cols {
 			for _, dir := range directions {
 				dx, dy := dir[0], dir[1]
-				if checkDirection(i, j, dx, dy) {
+				if checkDirection(i, j, dx, dy, word, grid) {
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
+
+func partTwo(grid [][]rune) int {
+	rows := len(grid)
+	cols := len(grid[0])
+	count := 0
+	directions := [][2]int{
+		{-1, -1}, {1, 1},
+		{-1, 1}, {1, -1},
+	}
+
+	for i := range rows {
+		for j := range cols {
+			if grid[i][j] == 'A' {
+				runesCount := map[rune]int{}
+				for _, dir := range directions {
+					nx, ny := i+dir[0], j+dir[1]
+					outOfBounds := nx < 0 || ny < 0 || nx >= rows || ny >= cols
+					if !outOfBounds && grid[nx][ny] != 'X' {
+						runesCount[grid[nx][ny]]++
+					}
+				}
+				// delete non-valid x-mas
+				if len(runesCount) != 2 || runesCount['M'] != 2 || runesCount['S'] != 2 {
+					delete(runesCount, 'M')
+					delete(runesCount, 'S')
+				}
+
+				if runesCount['M'] == 2 && runesCount['S'] == 2 {
 					count++
 				}
 			}
@@ -51,6 +89,6 @@ func main() {
 		fmt.Printf("Error loading grid: %v\n", err)
 		return
 	}
-	fmt.Printf("Part 1: %v\n", partOne(grid, "XMAS"))
-	fmt.Printf("Part 2: %v\n", partOne(grid, "MAS"))
+	// fmt.Printf("Part 1: %v\n", partOne(grid, "XMAS"))
+	fmt.Printf("Part 2: %v\n", partTwo(grid))
 }
