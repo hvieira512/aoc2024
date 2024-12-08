053778753fb6dbf70d4c6b47f578878e66ecce34
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-08 16:32:18 +0000

    Day 4 done\

--- cmd/day4/main.go
+++ cmd/day4/main.go
@@ -6,16 +6,18 @@ import (
 	u "github.com/hvieira512/aoc2024/cmd/utils"
 )
 
+func inBounds(x, y, n int) bool {
+	return x >= 0 && y >= 0 && x < n && y < n
+}
+
 func checkDirection(x, y, dx, dy int, word string, grid [][]rune) bool {
-	rows := len(grid)
-	cols := len(grid[0])
+	n := len(grid)
 	wordRunes := []rune(word)
 
 	for i := 0; i < len(word); i++ {
 		nx, ny := x+dx*i, y+dy*i
-		outOfBounds := nx < 0 || ny < 0 || nx >= rows || ny >= cols
 
-		if outOfBounds || grid[nx][ny] != wordRunes[i] {
+		if !inBounds(nx, ny, n) || grid[nx][ny] != wordRunes[i] {
 			return false
 		}
 	}
@@ -23,8 +25,7 @@ func checkDirection(x, y, dx, dy int, word string, grid [][]rune) bool {
 }
 
 func partOne(grid [][]rune, word string) int {
-	rows := len(grid)
-	cols := len(grid[0])
+	n := len(grid)
 	count := 0
 	directions := [][2]int{
 		{-1, 0}, {1, 0},
@@ -33,8 +34,8 @@ func partOne(grid [][]rune, word string) int {
 		{-1, 1}, {1, -1},
 	}
 
-	for i := range rows {
-		for j := range cols {
+	for i := range n {
+		for j := range n {
 			for _, dir := range directions {
 				dx, dy := dir[0], dir[1]
 				if checkDirection(i, j, dx, dy, word, grid) {
@@ -47,35 +48,33 @@ func partOne(grid [][]rune, word string) int {
 	return count
 }
 
-func partTwo(grid [][]rune) int {
-	rows := len(grid)
-	cols := len(grid[0])
-	count := 0
-	directions := [][2]int{
-		{-1, -1}, {1, 1},
-		{-1, 1}, {1, -1},
+func hasXmas(i, j, n int, grid [][]rune) bool {
+	if !(1 <= i && i < n-1 && 1 <= j && j < n-1) {
+		return false
 	}
 
-	for i := range rows {
-		for j := range cols {
-			if grid[i][j] == 'A' {
-				runesCount := map[rune]int{}
-				for _, dir := range directions {
-					nx, ny := i+dir[0], j+dir[1]
-					outOfBounds := nx < 0 || ny < 0 || nx >= rows || ny >= cols
-					if !outOfBounds && grid[nx][ny] != 'X' {
-						runesCount[grid[nx][ny]]++
-					}
-				}
-				// delete non-valid x-mas
-				if len(runesCount) != 2 || runesCount['M'] != 2 || runesCount['S'] != 2 {
-					delete(runesCount, 'M')
-					delete(runesCount, 'S')
-				}
+	if grid[i][j] != 'A' {
+		return false
+	}
 
-				if runesCount['M'] == 2 && runesCount['S'] == 2 {
-					count++
-				}
+	diag1 := string(grid[i-1][j-1]) + string(grid[i][j]) + string(grid[i+1][j+1])
+	diag2 := string(grid[i-1][j+1]) + string(grid[i][j]) + string(grid[i+1][j-1])
+
+	if (diag1 == "MAS" || diag1 == "SAM") && (diag2 == "MAS" || diag2 == "SAM") {
+		return true
+	}
+
+	return false
+}
+
+func partTwo(grid [][]rune) int {
+	count := 0
+	n := len(grid)
+
+	for i := range n {
+		for j := range n {
+			if grid[i][j] == 'A' && hasXmas(i, j, n, grid) {
+				count++
 			}
 		}
 	}
@@ -84,11 +83,8 @@ func partTwo(grid [][]rune) int {
 }
 
 func main() {
-	grid, err := u.ReadGrid("cmd/day4/example.txt")
-	if err != nil {
-		fmt.Printf("Error loading grid: %v\n", err)
-		return
-	}
+	grid, _ := u.ReadGrid("cmd/day4/input.txt")
+
 	// fmt.Printf("Part 1: %v\n", partOne(grid, "XMAS"))
 	fmt.Printf("Part 2: %v\n", partTwo(grid))
 }
