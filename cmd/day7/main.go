7b28f8ec79cf3c1fc002071a7cb84572112deee8
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-10 23:53:45 +0000

    day 7 done

--- cmd/day7/main.go
+++ cmd/day7/main.go
@@ -9,142 +9,130 @@ import (
 )
 
 type Equation struct {
-	test    int
+	target  int
 	numbers []int
 }
 
-func parseInput(lines []string) []Equation {
+func getEquations(lines []string) []Equation {
 	equations := []Equation{}
 
-	for i := range lines {
-		parts := strings.Split(lines[i], ": ")
-
+	for _, line := range lines {
+		parts := strings.Split(line, ": ")
 		test, _ := strconv.Atoi(parts[0])
-
-		numbersStr := strings.Fields(parts[1])
+		aux := strings.Fields(parts[1])
 		numbers := []int{}
-		for _, numberStr := range numbersStr {
-			number, _ := strconv.Atoi(numberStr)
-			numbers = append(numbers, number)
+		for _, numStr := range aux {
+			num, _ := strconv.Atoi(numStr)
+			numbers = append(numbers, num)
 		}
-		equations = append(equations, Equation{test: test, numbers: numbers})
+		equations = append(equations, Equation{target: test, numbers: numbers})
 	}
 
 	return equations
 }
 
-func getOperators(operators []string, length int) []string {
-	var result []string
-	var helper func(current string, level int)
+func main() {
+	utils.RenderDayHeader(7)
+	lines, _ := utils.ReadLines("cmd/day7/input.txt")
+	equations := getEquations(lines)
 
-	helper = func(current string, level int) {
-		if level == length {
-			result = append(result, current)
-			return
-		}
-		for _, op := range operators {
-			helper(current+op, level+1)
-		}
-	}
-
-	helper("", 0)
-	return result
-}
-
-func testEquation(equation Equation, ops []string) bool {
-	for _, opAttempt := range ops {
-		result := equation.numbers[0]
-		for i := 0; i < len(equation.numbers)-1; i++ {
-			if string(opAttempt[i]) == "+" {
-				result += equation.numbers[i+1]
-			} else if string(opAttempt[i]) == "*" {
-				result *= equation.numbers[i+1]
-			}
-		}
-		if result == equation.test {
-			return true
-		}
-	}
-
-	return false
+	fmt.Printf("Part 1: %v\n", partOne(equations))
+	fmt.Printf("Part 2: %v\n", partTwo(equations))
 }
 
 func partOne(equations []Equation) int {
 	result := 0
-	opsChars := []string{"+", "*"}
+	operators := []rune{'+', '*'}
 
 	for _, equation := range equations {
-		ops := getOperators(opsChars, len(equation.numbers)-1)
+		solved := false
+		combos := getOpsCombo(operators, len(equation.numbers)-1)
 
-		if testEquation(equation, ops) {
-			result += equation.test
-		}
-	}
-
-	return result
-}
-
-// ------------------
-// ---- PART TWO ----
-// ------------------
-func partTwo(equations []Equation) int {
-	result := 0
-	opsChars := []string{"+", "*", "||"}
-
-	for _, equation := range equations {
-		if equation.test != 7290 {
-			continue
-		}
-		fmt.Println(equation)
-		ops := getOperators(opsChars, len(equation.numbers)-1)
-
-		if testEquation(equation, ops) {
-			result += equation.test
-		} else if testEquationV2(equation, ops) {
-			result += equation.test
-		}
-	}
-
-	return result
-}
-
-func testEquationV2(equation Equation, ops []string) bool {
-	for _, opAttempt := range ops {
-		equation.numbers = updateEquation(equation.numbers, opAttempt)
-		result := equation.numbers[0]
-		for i := 0; i < len(equation.numbers)-1; i++ {
-			if string(opAttempt[i]) == "+" {
-				result += equation.numbers[i+1]
-			} else if string(opAttempt[i]) == "*" {
-				result *= equation.numbers[i+1]
+		for _, combo := range combos {
+			if testEquation(equation, combo) {
+				solved = true
 			}
 		}
-		if result == equation.test {
+		if solved {
+			result += equation.target
+			continue
+		}
+	}
+
+	return result
+}
+
+func getOpsCombo(operators []rune, length int) [][]rune {
+	var result [][]rune
+
+	var backtrack func(curr []rune)
+	backtrack = func(curr []rune) {
+		if len(curr) == length {
+			comboCopy := make([]rune, len(curr))
+			copy(comboCopy, curr)
+			result = append(result, comboCopy)
+			return
+		}
+
+		for _, op := range operators {
+			backtrack(append(curr, op))
+		}
+	}
+
+	backtrack([]rune{})
+	return result
+}
+
+func testEquation(equation Equation, combo []rune) bool {
+	result := equation.numbers[0]
+
+	for i, op := range combo {
+		switch op {
+		case '+':
+			result += equation.numbers[i+1]
+		case '*':
+			result *= equation.numbers[i+1]
+		}
+	}
+
+	return result == equation.target
+}
+
+// part two
+func partTwo(equations []Equation) int {
+	result := 0
+
+	for _, equation := range equations {
+		if canObtain(equation.target, equation.numbers) {
+			result += equation.target
+		}
+	}
+
+	return result
+}
+
+func canObtain(target int, array []int) bool {
+	if len(array) == 1 {
+		return target == array[0]
+	}
+
+	if target%array[len(array)-1] == 0 && canObtain(target/array[len(array)-1], array[:len(array)-1]) {
+		return true
+	}
+
+	if target > array[len(array)-1] && canObtain(target-array[len(array)-1], array[:len(array)-1]) {
+		return true
+	}
+
+	sTarget := strconv.Itoa(target)
+	sLast := strconv.Itoa(array[len(array)-1])
+
+	if len(sTarget) > len(sLast) && strings.HasSuffix(sTarget, sLast) {
+		newTarget, _ := strconv.Atoi(sTarget[:len(sTarget)-len(sLast)])
+		if canObtain(newTarget, array[:len(array)-1]) {
 			return true
 		}
 	}
+
 	return false
-}
-
-func updateEquation(numbers []int, op string) []int {
-	for i := 0; i < len(numbers)-1; i++ {
-		fmt.Println(op, i)
-		if op == "||" {
-			fmt.Println(numbers[i], numbers[i+1])
-			concatenated, _ := strconv.Atoi(fmt.Sprintf("%d%d", numbers[i], numbers[i+1]))
-			numbers[i] = concatenated
-			numbers = append(numbers[:i+1], numbers[i+2:]...)
-			i--
-		}
-	}
-	return numbers
-}
-
-func main() {
-	utils.RenderDayHeader(7)
-	lines, _ := utils.ReadLines("cmd/day7/example.txt")
-	equations := parseInput(lines)
-
-	// fmt.Printf("Part 1: %v\n", partOne(equations))
-	fmt.Printf("Part 2: %v\n", partTwo(equations))
 }
