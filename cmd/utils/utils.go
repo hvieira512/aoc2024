37c937dd7d14262857893ac91c5eef91ea003129
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-11 02:34:57 +0000

    chore: updated RenderHeader

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -5,15 +5,24 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
 func RenderDayHeader(day int) {
-	fmt.Println("-----------------------------")
-	fmt.Println("---- Advent of Code 2024 ----")
-	fmt.Println("-----------------------------")
-	fmt.Printf("----        Day %v        ----\n", day)
-	fmt.Println("-----------------------------")
+	if day < 10 {
+		fmt.Println("-----------------------------")
+		fmt.Println("---- Advent of Code 2024 ----")
+		fmt.Println("-----------------------------")
+		fmt.Printf("----        Day %v        ----\n", day)
+		fmt.Println("-----------------------------")
+	} else {
+		fmt.Println("-----------------------------")
+		fmt.Println("---- Advent of Code 2024 ----")
+		fmt.Println("-----------------------------")
+		fmt.Printf("---        Day %v        ----\n", day)
+		fmt.Println("-----------------------------")
+	}
 }
 
 func ReadLines(filename string) ([]string, error) {
@@ -28,7 +37,7 @@ func ReadLines(filename string) ([]string, error) {
 	return lines, nil
 }
 
-func ReadGrid(filename string) ([][]rune, error) {
+func ReadGridRune(filename string) ([][]rune, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -47,7 +56,6 @@ func ReadGrid(filename string) ([][]rune, error) {
 	}
 
 	return grid, nil
-
 }
 
 func DeleteAtIndex(slice []int, index int) []int {
@@ -61,4 +69,37 @@ func SliceExists(target []int, list [][]int) bool {
 		}
 	}
 	return false
+}
+
+func ReadGridInt(filename string) ([][]int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	grid := [][]int{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		row := []int{}
+		for _, char := range line {
+			num, err := strconv.Atoi(string(char))
+			if err != nil {
+				return nil, err
+			}
+			row = append(row, num)
+		}
+		grid = append(grid, row)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return grid, nil
+}
+
+func InBoundsGrid(x, y, n int) bool {
+	return x >= 0 && y >= 0 && x < n && y < n
 }
