33379e42bf64650a460e90d6ca0ee281d6ac5281
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-11 16:59:58 +0000

    day 10 done

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 
 	u "github.com/hvieira512/aoc2024/cmd/utils"
@@ -82,4 +83,58 @@ func main() {
 	grid, _ := u.ReadGridInt("cmd/day10/input.txt")
 
 	fmt.Printf("Part 1: %v\n", partOne(grid))
+	fmt.Printf("Part 2: %v\n", partTwo(grid))
+}
+
+func partTwo(grid [][]int) int {
+	rows := len(grid)
+	cols := len(grid[0])
+
+	trailheads := getTrailHeads(grid, rows, cols)
+
+	result := 0
+	for _, start := range trailheads {
+		result += score2(grid, start[0], start[1])
+	}
+	return result
+}
+
+func score2(grid [][]int, r, c int) int {
+	rows := len(grid)
+	cols := len(grid[0])
+
+	q := list.New()
+	q.PushBack([2]int{r, c})
+	seen := make(map[[2]int]int)
+	seen[[2]int{r, c}] = 1
+	trails := 0
+
+	for q.Len() > 0 {
+		elem := q.Front()
+		q.Remove(elem)
+		current := elem.Value.([2]int)
+		cr, cc := current[0], current[1]
+
+		if grid[cr][cc] == 9 {
+			trails += seen[[2]int{cr, cc}]
+		}
+
+		for _, dir := range [][2]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}} {
+			nr, nc := cr+dir[0], cc+dir[1]
+			if nr < 0 || nc < 0 || nr >= rows || nc >= cols {
+				continue
+			}
+			if grid[nr][nc] != grid[cr][cc]+1 {
+				continue
+			}
+			if _, exists := seen[[2]int{nr, nc}]; exists {
+				seen[[2]int{nr, nc}] += seen[[2]int{cr, cc}]
+				continue
+			}
+			seen[[2]int{nr, nc}] = seen[[2]int{cr, cc}]
+			q.PushBack([2]int{nr, nc})
+		}
+	}
+
+	return trails
 }
