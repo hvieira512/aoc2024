717ed3551d2ad64082b228cf77fa4ffe0fc8aa00
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-11 18:32:34 +0000

    day 11 part 2 done

--- cmd/day11/main.go
+++ cmd/day11/main.go
@@ -2,72 +2,69 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 	"strings"
 
 	"github.com/hvieira512/aoc2024/cmd/utils"
 )
 
-func getStones(line string) []int64 {
-	stones := []int64{}
-	stonesStr := strings.Split(line, " ")
+func main() {
+	utils.RenderDayHeader(11)
+	lines, _ := utils.ReadLines("cmd/day11/input.txt")
+	stones := getStones(lines)
+
+	fmt.Printf("Part 1: %v\n", solve(stones, 25))
+	fmt.Printf("Part 2: %v\n", solve(stones, 75))
+}
+
+func getStones(lines []string) []int {
+	stones := []int{}
+	stonesStr := strings.Split(lines[0], " ")
 	for _, str := range stonesStr {
-		stone, _ := strconv.ParseInt(str, 10, 64)
+		stone, _ := strconv.Atoi(str)
 		stones = append(stones, stone)
 	}
 	return stones
 }
 
-func blink(stones []int64) []int64 {
-	newStones := make([]int64, 0, len(stones)*2)
+var memo = map[string]int{}
 
-	for _, stone := range stones {
-		numDigits := lenNum(stone)
+func count(stone, steps int) int {
+	key := fmt.Sprintf("%d-%d", stone, steps)
 
-		if stone == 0 {
-			newStones = append(newStones, 1)
-			continue
-		}
-
-		if numDigits%2 == 0 {
-			mid := numDigits / 2
-			divisor := int64(math.Pow10(mid))
-			left := stone / divisor
-			right := stone % divisor
-			newStones = append(newStones, left, right)
-			continue
-		}
-		newStones = append(newStones, stone*2024)
+	if val, exists := memo[key]; exists {
+		return val
 	}
 
-	return newStones
-}
-
-func lenNum(i int64) int {
-	if i == 0 {
+	if steps == 0 {
 		return 1
 	}
-	count := 0
-	for i != 0 {
-		i /= 10
-		count++
+
+	var result int
+	if stone == 0 {
+		result = count(1, steps-1)
+	} else {
+		numStr := strconv.Itoa(stone)
+		length := len(numStr)
+		if length%2 == 0 {
+			mid := length / 2
+			left, _ := strconv.Atoi(numStr[:mid])
+			right, _ := strconv.Atoi(numStr[mid:])
+			result = count(left, steps-1) + count(right, steps-1)
+		} else {
+			result = count(stone*2024, steps-1)
+		}
 	}
-	return count
+
+	memo[key] = result
+	return result
 }
 
-func solve(stones []int64, blinks int) int {
-	for i := 0; i < blinks; i++ {
-		stones = blink(stones)
+func solve(stones []int, blinks int) int {
+	total := 0
+	for _, stone := range stones {
+		total += count(stone, blinks)
 	}
-	return len(stones)
-}
 
-func main() {
-	utils.RenderDayHeader(11)
-	lines, _ := utils.ReadLines("cmd/day11/input.txt")
-	stones := getStones(lines[0])
-
-	// fmt.Printf("Part 1: %v\n", solve(stones, 25))
-	fmt.Printf("Part 2: %v\n", solve(stones, 75))
+	return total
 }
