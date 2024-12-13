a756a4a738fbf0939e7a397c80caab8dfa4f4ff5
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-13 19:10:04 +0000

    day 13 part 1

--- cmd/day13/main.go
+++ cmd/day13/main.go
@@ -3,21 +3,82 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/hvieira512/aoc2024/cmd/utils"
 )
 
+type ClawMachine struct {
+	Buttons [2][2]int
+	Prize   [2]int
+}
+
 func main() {
 	utils.RenderDayHeader(13)
-	lines, err := utils.ReadLines("cmd/day13/example.txt")
+	lines, err := utils.ReadLines("cmd/day13/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	machines := getMachines(lines)
 
-	fmt.Printf("Part 1: %v\n", partOne(lines))
-	// fmt.Printf("Part 2: %v\n", partTwo(lines))
+	fmt.Printf("Part 1: %v\n", partOne(machines))
 }
 
-func partOne(lines []string) int {
-	panic("unimplemented")
+func getMachines(lines []string) []ClawMachine {
+	var machines []ClawMachine
+	rows := len(lines)
+
+	for r := 0; r < rows; r += 3 {
+		if len(lines[r]) == 0 {
+			r++
+		}
+
+		var machine ClawMachine
+		for i := 0; i <= 1; i++ {
+			buttonAux := strings.Split(lines[r+i][10:], ": ")[0]
+			coordsAux := strings.Split(buttonAux, ", ")
+			x, _ := strconv.Atoi(strings.Split(coordsAux[0], "+")[1])
+			y, _ := strconv.Atoi(strings.Split(coordsAux[1], "+")[1])
+			machine.Buttons[i] = [2]int{x, y}
+		}
+
+		// Get prize
+		prizeAux := strings.Split(lines[r+2][7:], ": ")[0]
+		coordsAux := strings.Split(prizeAux, ", ")
+		x, _ := strconv.Atoi(strings.Split(coordsAux[0], "=")[1])
+		y, _ := strconv.Atoi(strings.Split(coordsAux[1], "=")[1])
+		machine.Prize = [2]int{x, y}
+
+		machines = append(machines, machine)
+	}
+
+	return machines
+}
+
+func partOne(machines []ClawMachine) int {
+	result := 0
+	for _, machine := range machines {
+		result += findCheapest(machine.Buttons, machine.Prize)
+	}
+	return result
+}
+
+func findCheapest(A [2][2]int, B [2]int) int {
+	cheapest := [2]int{}
+	maxPresses, minTokens := 100, -1
+
+	for x := 1; x <= maxPresses; x++ {
+		for y := 1; y <= maxPresses; y++ {
+			if x*A[0][0]+y*A[1][0] == B[0] && x*A[0][1]+y*A[1][1] == B[1] {
+				tokens := x*3 + y
+				if minTokens == -1 || (tokens < minTokens) {
+					minTokens = x*3 + y
+					cheapest = [2]int{x, y}
+				}
+			}
+		}
+	}
+
+	return cheapest[0]*3 + cheapest[1]
 }
