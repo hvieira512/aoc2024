107cd2783dcf603cb31948e455e198accd5d1701
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-12-15 00:59:19 +0000

    day 14 bonus 😃

--- cmd/day14/main.go
+++ cmd/day14/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 
@@ -14,25 +15,25 @@ type Robot struct {
 }
 
 const (
-	Width   = 101
-	Height  = 103
-	Seconds = 100
+	Width    = 101
+	Height   = 103
+	Seconds  = 100
+	Filename = "input"
 )
+
+var midX int = (Width - 1) / 2
+var midY int = (Height - 1) / 2
 
 func parseRobots(lines []string) []Robot {
 	var robots []Robot
-	n := len(lines)
-
-	for i := range n {
-		robots = append(robots, parseRobot(lines[i]))
+	for _, line := range lines {
+		robots = append(robots, parseRobot(line))
 	}
-
 	return robots
 }
 
 func parseRobot(line string) Robot {
 	var robot Robot
-
 	re := regexp.MustCompile(`-?\d+`)
 	matches := re.FindAllString(line, -1)
 
@@ -49,19 +50,31 @@ func parseRobot(line string) Robot {
 
 func main() {
 	utils.RenderDayHeader(14)
-	lines, _ := utils.ReadLines("cmd/day14/input.txt")
+	lines, _ := utils.ReadLines("cmd/day14/" + Filename + ".txt")
 	robots := parseRobots(lines)
 
 	fmt.Printf("Part 1: %v\n", partOne(robots))
+	fmt.Printf("Part 2: %v\n", partTwo(robots))
 }
 
-func partOne(robots []Robot) int {
+func moveRobot(robot Robot, seconds int) [2]int {
+	x := (robot.pos[0] + robot.vel[0]*seconds) % Width
+	if x < 0 {
+		x += Width
+	}
+	y := (robot.pos[1] + robot.vel[1]*seconds) % Height
+	if y < 0 {
+		y += Height
+	}
+
+	return [2]int{x, y}
+}
+
+func countRobotsInQuads(robots []Robot, seconds int) (int, int, int, int) {
 	q1, q2, q3, q4 := 0, 0, 0, 0
-	midX, midY := (Width-1)/2, (Height-1)/2
 
 	for _, robot := range robots {
-		robot.pos = moveRobot(robot)
-
+		robot.pos = moveRobot(robot, seconds)
 		switch {
 		case robot.pos[0] > midX && robot.pos[1] > midY:
 			q1++
@@ -74,21 +87,50 @@ func partOne(robots []Robot) int {
 		}
 	}
 
+	return q1, q2, q3, q4
+}
+
+func partOne(robots []Robot) int {
+	q1, q2, q3, q4 := countRobotsInQuads(robots, Seconds)
+
 	return q1 * q2 * q3 * q4
 }
 
-func moveRobot(robot Robot) [2]int {
-	px, py := robot.pos[0], robot.pos[1]
-	vx, vy := robot.vel[0], robot.vel[1]
+func partTwo(robots []Robot) int {
+	minSF := math.Inf(1)
+	bestIteration := -1
 
-	x := (px + vx*Seconds) % Width
-	if x < 0 {
-		x += Width
-	}
-	y := (py + vy*Seconds) % Height
-	if y < 0 {
-		y += Height
+	for second := range Width * Height {
+		q1, q2, q3, q4 := countRobotsInQuads(robots, second)
+
+		sf := float64(q1 * q2 * q3 * q4)
+		if sf < minSF {
+			minSF = sf
+			bestIteration = second
+		}
 	}
 
-	return [2]int{x, y}
+	displayGrid(robots, bestIteration)
+
+	return bestIteration
+}
+
+func displayGrid(robots []Robot, seconds int) {
+	grid := make([][]string, Height)
+	for i := range grid {
+		grid[i] = make([]string, Width)
+		for j := range grid[i] {
+			grid[i][j] = "."
+		}
+	}
+
+	for _, robot := range robots {
+		robot.pos = moveRobot(robot, seconds)
+		x, y := robot.pos[0], robot.pos[1]
+		grid[y][x] = "#"
+	}
+
+	for _, row := range grid {
+		fmt.Println(row)
+	}
 }
